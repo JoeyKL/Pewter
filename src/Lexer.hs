5a0a4f556689868cc82f0411b28baf860df3aa91
JoeyKL <joey@kellison-linn.org>
2016-10-29 15:39:36 -0700

    Add additional megaparsec imports

--- src/Lexer.hs
+++ src/Lexer.hs
@@ -1,4 +1,9 @@
 module Lexer where
 
 import qualified Data.Text             as T
-import           Text.Megaparsec.Lexer
+import           Text.Megaparsec
+import           Text.Megaparsec.Expr
+import qualified Text.Megaparsec.Lexer as L
+import           Text.Megaparsec.Text
+
+lex :: T.Text -> Maybe [Token]
