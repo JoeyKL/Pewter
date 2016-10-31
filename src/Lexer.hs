a7b9fdd4985bb04dce156d4bcda5d5fdbac0126f
JoeyKL <joey@kellison-linn.org>
2016-10-31 14:00:41 -0700

    Add Token type and lex type signature

--- src/Lexer.hs
+++ src/Lexer.hs
@@ -5,3 +5,13 @@ import           Text.Megaparsec
 import           Text.Megaparsec.Expr
 import qualified Text.Megaparsec.Lexer as L
 import           Text.Megaparsec.Text
+
+data Token
+  = Identifier T.Text
+  | Operator T.Text
+  | Equals
+  | TypeEquals
+  | TypeOr
+  | EndOfLine
+
+lex :: T.Text -> CompilerError [Token]
