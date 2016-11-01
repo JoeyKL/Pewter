e682b549d3ce2bb6e83d4fb7e0a909061d7dd708
JoeyKL <joey@kellison-linn.org>
2016-11-01 15:37:33 -0700

    Add initial lexer

--- src/Lexer.hs
+++ src/Lexer.hs
@@ -1,24 +1,95 @@
-module Lexer where
+module Pewter.Lexer where
 
-import           Main
+import           Control.Monad         (void)
+import           Data.Functor
 import qualified Data.Text             as T
-import           Text.Megaparsec
+import           Error
+--import           Text.Megaparsec
+import           Text.Megaparsec       hiding (Token, token)
+import           Text.Megaparsec.Char
 import           Text.Megaparsec.Expr
 import qualified Text.Megaparsec.Lexer as L
 import           Text.Megaparsec.Text
 
 data Token
   = Identifier T.Text
-  | Operator T.Text
+  | IntegerLiteral Integer
+  | OpenParen
+  | CloseParen
+  | OpenBrace
+  | CloseBrace
   | Equals
   | TypeEquals
   | TypeOr
   | TypeSignature
   | Let
-  | In
-  | EndOfLine
+  | Semicolon
+  deriving (Show)
 
-lex :: T.Text -> CompilerError [Token]
-lex = undefined
+main :: T.Text -> CompilerResult [Token]
+main source = case parse lexer "Input file" source of
+  Left err     -> Failure (Error.ParseError err)
+  Right result -> Success result
 
-whitespace = L.space
+lexer :: Parser [Token]
+lexer = many (lexeme token)
+
+blank = void spaceChar
+lineComment = L.skipLineComment "//"
+blockComment = L.skipBlockCommentNested "/*" "*/"
+
+whitespace :: Parser ()
+whitespace = L.space blank lineComment blockComment
+
+lexeme :: Parser a -> Parser a
+lexeme = L.lexeme whitespace
+
+token :: Parser Token
+token = choice
+  [word
+  ,integerLiteral
+  ,equals
+  ,openParen
+  ,closeParen
+  ,openBrace
+  ,closeBrace
+  ,typeEquals
+  ,typeOr
+  ,semicolon
+  ]
+
+word :: Parser Token
+word = reservedWords <$> parseWord where
+  parseWord :: Parser String
+  parseWord = (:) <$> letterChar <*> many alphaNumChar
+
+integerLiteral :: Parser Token
+integerLiteral = IntegerLiteral <$> L.integer
+
+equals :: Parser Token
+equals =  Equals <$ string "="
+
+openParen :: Parser Token
+openParen =  OpenParen <$ string "("
+
+closeParen :: Parser Token
+closeParen =  CloseParen <$ string ")"
+
+openBrace :: Parser Token
+openBrace =  OpenBrace <$ string "{"
+
+closeBrace :: Parser Token
+closeBrace =  CloseBrace <$ string "}"
+
+typeEquals :: Parser Token
+typeEquals = TypeEquals <$ string ":="
+
+typeOr :: Parser Token
+typeOr = TypeOr <$ string "|"
+
+semicolon :: Parser Token
+semicolon = Semicolon <$ string ";"
+
+reservedWords :: String -> Token
+reservedWords "let" = Let
+reservedWords word  = Identifier $ T.pack word
