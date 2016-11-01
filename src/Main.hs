9e4e2b24893baac0430823f736b2d346326b862e
JoeyKL <joey@kellison-linn.org>
2016-11-01 15:36:46 -0700

    Modify to use error types in Error.hs

--- src/Main.hs
+++ src/Main.hs
@@ -1,20 +1,13 @@
 module Main where
 
-import qualified Data.Text     as T
-import           Pewter.Lexer  as Lexer
-import           Pewter.Parser as Parser
-
-data CompilerResult a
-  = Success a
-  | Failure CompilerError
-
-data CompilerError
-  = ParseError
+import qualified Data.Text as T
+import           Error     as Error
+import           Lexer     as Lexer
 
 main :: IO ()
 main = putStrLn "Yes, this the Pewter compiler, no, it doesn't work yet."
 
-compile :: T.Text -> CompilerResult T.Text
+compile :: T.Text -> Error.CompilerResult T.Text
 compile =
   Lexer.lex >=>
   Parser.parse >=>
